d2cb6b66591885330f467d831db5efaa92b4cc5c
Liam Keegan <liam@keegan.ch>
2022-09-27 17:02:21 +0200

    add qt gui tests for results list widget

--- src/motor_task_prototype/results_widget.py
+++ src/motor_task_prototype/results_widget.py
@@ -10,73 +10,71 @@ from PyQt5 import QtWidgets
 
 
 class ResultListWidget(QtWidgets.QWidget):
-    experiment: Optional[TrialHandlerExt] = None
-    condition_to_trials: DefaultDict[int, List[int]] = defaultdict(list)
-    trial_to_condition: Dict[int, int] = dict()
+    _experiment: Optional[TrialHandlerExt] = None
+    _condition_to_trials: DefaultDict[int, List[int]] = defaultdict(list)
+    _trial_to_condition: Dict[int, int] = dict()
 
-    def __init__(self, parent: QtWidgets.QWidget):
+    def __init__(self, parent: Optional[QtWidgets.QWidget] = None):
         super().__init__(parent)
         outer_layout = QtWidgets.QVBoxLayout()
         group_box = QtWidgets.QGroupBox("Results")
         outer_layout.addWidget(group_box)
         inner_layout = QtWidgets.QGridLayout()
         group_box.setLayout(inner_layout)
-        self.widget_list_trials = QtWidgets.QListWidget()
-        self.widget_list_trials.currentRowChanged.connect(self.row_changed)
-        self.widget_list_trials.itemDoubleClicked.connect(
-            self.btn_display_trial_clicked
-        )
-        inner_layout.addWidget(self.widget_list_trials, 0, 0, 1, 2)
-        self.btn_display_trial = QtWidgets.QPushButton("Display Trial")
-        self.btn_display_trial.clicked.connect(self.btn_display_trial_clicked)
-        inner_layout.addWidget(self.btn_display_trial, 1, 0)
-        self.btn_display_condition = QtWidgets.QPushButton("Display Condition")
-        self.btn_display_condition.clicked.connect(self.btn_display_condition_clicked)
-        inner_layout.addWidget(self.btn_display_condition, 1, 1)
+        self._list_trials = QtWidgets.QListWidget()
+        self._list_trials.currentRowChanged.connect(self._row_changed)
+        self._list_trials.itemDoubleClicked.connect(self._btn_display_trial_clicked)
+        inner_layout.addWidget(self._list_trials, 0, 0, 1, 2)
+        self._btn_display_trial = QtWidgets.QPushButton("Display Trial")
+        self._btn_display_trial.clicked.connect(self._btn_display_trial_clicked)
+        inner_layout.addWidget(self._btn_display_trial, 1, 0)
+        self._btn_display_condition = QtWidgets.QPushButton("Display Condition")
+        self._btn_display_condition.clicked.connect(self._btn_display_condition_clicked)
+        inner_layout.addWidget(self._btn_display_condition, 1, 1)
         self.setLayout(outer_layout)
-        self.row_changed()
+        self._row_changed()
 
-    def is_valid(self, row: int) -> bool:
-        return self.have_results() and 0 <= row < self.widget_list_trials.count()
+    def _is_valid(self, row: int) -> bool:
+        return self.have_results() and 0 <= row < self._list_trials.count()
 
-    def row_changed(self) -> None:
-        valid = self.is_valid(self.widget_list_trials.currentRow())
-        self.btn_display_trial.setEnabled(valid)
-        self.btn_display_condition.setEnabled(valid)
+    def _row_changed(self) -> None:
+        valid = self._is_valid(self._list_trials.currentRow())
+        self._btn_display_trial.setEnabled(valid)
+        self._btn_display_condition.setEnabled(valid)
 
-    def btn_display_trial_clicked(self) -> None:
-        row = self.widget_list_trials.currentRow()
-        if not self.is_valid(row):
+    def _btn_display_trial_clicked(self) -> None:
+        row = self._list_trials.currentRow()
+        if not self._is_valid(row):
             return
-        display_results(self.experiment, [row])
+        display_results(self._experiment, [row])
 
-    def btn_display_condition_clicked(self) -> None:
-        row = self.widget_list_trials.currentRow()
-        if not self.is_valid(row):
+    def _btn_display_condition_clicked(self) -> None:
+        row = self._list_trials.currentRow()
+        if not self._is_valid(row):
             return
-        condition = self.trial_to_condition[row]
-        display_results(self.experiment, self.condition_to_trials[condition])
+        condition = self._trial_to_condition[row]
+        display_results(self._experiment, self._condition_to_trials[condition])
 
     def clear_results(self) -> None:
-        self.experiment = None
-        self.trial_to_condition.clear()
-        self.condition_to_trials.clear()
-        self.widget_list_trials.clear()
+        self._experiment = None
+        self._trial_to_condition.clear()
+        self._condition_to_trials.clear()
+        self._list_trials.clear()
 
     def have_results(self) -> bool:
-        return self.experiment is not None and self.experiment.finished
+        return self._experiment is not None and self._experiment.finished
 
     def set_results(self, experiment: TrialHandlerExt) -> None:
         self.clear_results()
-        self.experiment = experiment
+        self._experiment = experiment
         # assume 1 repetition of experiment
         i_rep = 0
-        self.widget_list_trials.clear()
+        self._list_trials.clear()
         if experiment.finished is False:
             return
         for trial_index, condition_index in enumerate(experiment.sequenceIndices):
-            self.widget_list_trials.addItem(
+            self._list_trials.addItem(
                 f"Trial {trial_index} [Condition {condition_index[i_rep]}]"
             )
-            self.condition_to_trials[condition_index[i_rep]].append(trial_index)
-            self.trial_to_condition[trial_index] = condition_index[i_rep]
+            self._condition_to_trials[condition_index[i_rep]].append(trial_index)
+            self._trial_to_condition[trial_index] = condition_index[i_rep]
