331a97b27975b7d31d5a3565390679caf0c9c1bc
Liam Keegan <liam@keegan.ch>
2022-11-14 21:18:46 +0100

    add `cursor_size` field to trial
    - resolves #126

--- tests/test_trial.py
+++ tests/test_trial.py
@@ -31,7 +31,7 @@ def test_describe_trials() -> None:
 
 def test_default_trial() -> None:
     trial = mtptrial.default_trial()
-    assert len(trial) == 18
+    assert len(trial) == 19
     assert isinstance(trial["target_indices"], str)
     assert len(trial["target_indices"].split(" ")) == trial["num_targets"]
 
@@ -57,6 +57,7 @@ def test_import_trial() -> None:
         "central_target_size": 0.01,
         "play_sound": True,
         "show_cursor": False,
+        "cursor_size": 0.0123,
         "show_cursor_path": True,
         "automove_cursor_to_center": True,
         "cursor_rotation_degrees": 45,
