4a5645db8f6ff81468be8562853f40ca73660a8d
Liam Keegan <liam@keegan.ch>
2023-05-30 10:11:14 +0200

    Update docs for first release
    - update installation instructions
    - update and rename install/update windows batch files
    - update all screenshots, resolves #178

--- misc/UpdatePrototype.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Program Files\PsychoPy\python.exe" -m pip install https://github.com/ssciwr/vstt/releases/download/latest/vstt-latest-py3-none-any.whl
---
+++ misc/UpdateVSTT.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files\PsychoPy\python.exe" -m pip install --upgrade vstt
