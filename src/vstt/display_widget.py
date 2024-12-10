b31edea3556c911131045823b4887d3214624b59
ZoeLi0525 <1558079116@qq.com>
2024-12-10 15:53:27 +0100

    add tree hierarchy to reconstruct the display options

--- src/vstt/display_widget.py
+++ src/vstt/display_widget.py
@@ -6,6 +6,7 @@ from psychopy.visual.window import Window
 from qtpy import QtCore
 from qtpy import QtWidgets
 
+from vstt.display import display_options_groups
 from vstt.display import display_options_labels
 from vstt.experiment import Experiment
 
@@ -21,20 +22,58 @@ class DisplayOptionsWidget(QtWidgets.QWidget):
         self._experiment: Experiment = Experiment()
         self._widgets: dict[str, QtWidgets.QCheckBox] = {}
 
+        # outer_layout = QtWidgets.QVBoxLayout()
+        # group_box = QtWidgets.QGroupBox("Display Options")
+        # scroll_area = QtWidgets.QScrollArea()
+        # scroll_area.setWidgetResizable(True)
+        # scroll_area.setWidget(group_box)
+        # outer_layout.addWidget(scroll_area)
+        # inner_layout = QtWidgets.QVBoxLayout()
+        # group_box.setLayout(inner_layout)
+        # labels = display_options_labels()
+        # for _row_index, key in enumerate(labels.keys()):
+        #     checkbox = QtWidgets.QCheckBox(f"{labels[key]}", self)
+        #     inner_layout.addWidget(checkbox)
+        #     checkbox.clicked.connect(self._update_value_callback(key))
+        #     self._widgets[key] = checkbox
+        # self.setLayout(outer_layout)
+
+        # Main layout
         outer_layout = QtWidgets.QVBoxLayout()
-        group_box = QtWidgets.QGroupBox("Display Options")
-        scroll_area = QtWidgets.QScrollArea()
-        scroll_area.setWidgetResizable(True)
-        scroll_area.setWidget(group_box)
-        outer_layout.addWidget(scroll_area)
-        inner_layout = QtWidgets.QVBoxLayout()
-        group_box.setLayout(inner_layout)
+
+        # Tree widget for collapsible categories
+        tree_widget = QtWidgets.QTreeWidget()
+        tree_widget.setHeaderHidden(True)  # Hide header
+        outer_layout.addWidget(tree_widget)
+
+        # Labels for display options
         labels = display_options_labels()
-        for _row_index, key in enumerate(labels.keys()):
-            checkbox = QtWidgets.QCheckBox(f"{labels[key]}", self)
-            inner_layout.addWidget(checkbox)
-            checkbox.clicked.connect(self._update_value_callback(key))
-            self._widgets[key] = checkbox
+        collapsible_labels = display_options_groups()
+        collapsible_keys = []
+
+        # Add collapsible groups
+        for group_title, keys in collapsible_labels.items():
+            parent_item = QtWidgets.QTreeWidgetItem(tree_widget)
+            parent_item.setText(0, group_title)
+
+            for key in keys:
+                collapsible_keys.append(key)
+                if key in labels:
+                    child_item = QtWidgets.QTreeWidgetItem(parent_item)
+                    checkbox = QtWidgets.QCheckBox(f"{labels[key]}")
+                    tree_widget.setItemWidget(child_item, 0, checkbox)
+                    checkbox.clicked.connect(self._update_value_callback(key))
+                    self._widgets[key] = checkbox
+
+        # Add non-collapsible options
+        for key, label in labels.items():
+            if key not in collapsible_keys:
+                item = QtWidgets.QTreeWidgetItem(tree_widget)
+                checkbox = QtWidgets.QCheckBox(f"{label}")
+                tree_widget.setItemWidget(item, 0, checkbox)
+                checkbox.clicked.connect(self._update_value_callback(key))
+                self._widgets[key] = checkbox
+
         self.setLayout(outer_layout)
 
     def _update_value_callback(self, key: str) -> Callable[[bool], None]:
